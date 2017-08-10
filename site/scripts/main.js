d268923d5a908b7989b7eef8fcbb6264b07aee92
Mladen Mijatov <meaneye.rcf@gmail.com>
2017-08-10 18:18:53 +0200

    Created complex popup dialog behavior.

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -46,11 +46,70 @@ Site.is_mobile = function() {
 };
 
 /**
+ * Handle clicking on submit button in contact form dialog.
+ * @param object event
+ */
+Site.handle_submit_click = function(event) {
+	event.preventDefault();
+	Caracal.ContactForm.list[0]._form.submit();
+};
+
+/**
+ * Handle clicking on download link.
+ * @param object event
+ */
+Site.handle_download_click = function(event) {
+	// prevent default link behavior
+	event.preventDefault();
+
+	// store download URL for later
+	Site.download_url = event.target.dataset.download;
+
+	// configure dialog
+	Site.form_dialog.set_title(event.target.dataset.title);
+	Site.submit_button.innerHtml = event.target.dataset.button;
+
+	// show contact form
+	Site.form_dialog.open();
+};
+
+/**
+ * Handle successful data submision.
+ * @param object response_data
+ */
+Site.handle_submit_success = function(response_data) {
+	// make sure no server side errors occurred
+	if (!data.error)
+		window.location = Site.download_url;
+};
+
+/**
  * Function called when document and images have been completely loaded.
  */
 Site.on_load = function() {
 	if (Site.is_mobile())
 		Site.mobile_menu = new Caracal.MobileMenu();
+
+	// storage variable for download link
+	Site.download_url = null;
+
+	// create dialog for contact form
+	Site.submit_button = document.createElement('a');
+	Site.submit.addEventListener('click', Site.handle_submit_click);
+
+	Site.form_dialog = new Caracal.Dialog();
+	Site.form_dialog
+		.set_clear_on_close(false)
+		.set_content_from_dom('div#contact_form')
+		.add_control(Site.submit_button);
+
+	// connect download links with dialog
+	var links = document.querySelectorAll('a.download');
+	for (var i=0,count=links.length; i<count; i++)
+		links[i].addEventListener('click', Site.handle_download_click);
+
+	// connect form successful submision to download handler
+	Caracal.ContactForm.list[0].events.connect('submit-success', Site.handle_submit_success);
 };
 
 
